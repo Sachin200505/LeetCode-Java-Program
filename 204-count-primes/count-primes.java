7802cbf338c1b2d0f4e104f06c56e0967162d1c8
Sachin Sundar S <160867272+Sachin200505@users.noreply.github.com>
2025-07-29 16:01:28 +0530

    Time: 72 ms (97.20%) | Memory: 49.2 MB (70.72%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -2,21 +2,20 @@ class Solution {
     public int countPrimes(int n) {
         if (n <= 2) return 0;
         boolean[] prime = new boolean[n];
-        for (int i = 2; i < n; i++) {
-            prime[i] = true; 
+        for (int i = 3; i < n; i += 2) {
+            prime[i] = true;
         }
-        for (int i = 2; i * i < n; i++) {
+        prime[2] = true;
+        for (int i = 3; i * i < n; i += 2) {
             if (prime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    prime[j] = false; 
+                for (int j = i * i; j < n; j += 2 * i) {
+                    prime[j] = false;
                 }
             }
         }
         int c = 0;
         for (int i = 2; i < n; i++) {
-            if (prime[i] == true) {
-                c++;
-            }
+            if (prime[i]) c++;
         }
         return c;
     }
