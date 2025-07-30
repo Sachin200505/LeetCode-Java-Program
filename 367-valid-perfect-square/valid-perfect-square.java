f96acfbf8887d2b0aa7d0cb5aaf7314e97670676
Sachin Sundar S <160867272+Sachin200505@users.noreply.github.com>
2025-07-30 22:21:59 +0530

    Time: 1 ms (19.84%) | Memory: 40.8 MB (19.11%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,16 +1,8 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        int n=0;
-        int flag=0;
-        for(int i=0;i<=num/2;i++){
-            if((i*i)==num){
-               return true;
-            }
-            if(i*i>num){
-                break;
-            }
-        }
-        if(num==1){
+        int n=(int)Math.sqrt(num);
+        System.out.println(n);
+        if((n*n)==num){
             return true;
         }
         else{
